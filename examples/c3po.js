d9292938213383e54fd711448ca4a2de8b39adfe
Chris Mytton <self@hecticjeff.net>
2011-07-11 00:31:34 +0100

    add desc to example

--- examples/c3po.js
+++ examples/c3po.js
@@ -4,10 +4,12 @@ var c3po = bot.createBot('c3po 1.0.0');
 
 c3po.use(bot.cli());
 
+c3po.desc('thanks');
 c3po.hear(/thanks/, function(message) {
   message.say("Oh you're perfectly welcome, sir.");
 });
 
+c3po.desc('tweets');
 c3po.hear(/tweets/, function(message) {
   this.get('http://api.twitter.com/1/statuses/user_timeline.json?screen_name=c3po', function(body) {
     body.forEach(function(tweet) {
